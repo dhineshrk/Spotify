355f8b46a828d1d59148639c6ba35f593f9282f7
dhineshrk <dhineshrk77@gmail.com>
2025-01-15 18:20:35 +0530

    No issues

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
-import { SignedIn, SignedOut, SignOutButton } from "@clerk/clerk-react";
+import {  SignedOut, UserButton } from "@clerk/clerk-react";
 import SignInOAuthButtons from "./SignInOAuthButtons";
 import { LayoutDashboardIcon } from "lucide-react";
 import { useAuthStore } from "@/stores/useAuthStore";
+import { buttonVariants } from "./ui/button";
+import { cn } from "@/lib/utils";
 
 const Topbar = () => {
 
@@ -13,22 +15,30 @@ const Topbar = () => {
   return (
     <div className="flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75 backdrop-blur-md z-10">
         <div className="flex gap-2 items-center">
+            <img className="size-8" src="/spotify.png" alt="Spotify Logo" />
             Spotify
         </div>
         <div className="flex items-center gap-4">
             {isAdmin && (
-                <Link to={"/admin"}>
+                <Link to={"/admin"} className={
+                    cn(
+                        buttonVariants({variant: "outline"})
+                    )
+                } >
                     <LayoutDashboardIcon className="w-4 h-4 mr-2" />
                     Admin Dashboard
                 </Link>
             )}
 
-            <SignedIn>
-                <SignOutButton />
-            </SignedIn>
+            
+
+
+
             <SignedOut>
                 <SignInOAuthButtons />
             </SignedOut>
+
+            <UserButton />
         </div>
     </div>
   )
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,24 @@
 import Topbar from '@/components/Topbar'
+import { useMusicStore } from '@/stores/useMusicStore'
+import { useEffect } from 'react';
 
 const Home = () => {
+
+  const {fetchFeaturedSongs, fetchMadeForYouSongs, fetchTrendingSongs, isLoading, madeForYouSongs, featuredSongs, trendingSongs } =  useMusicStore();
+
+  useEffect(() => {
+    fetchFeaturedSongs();
+    fetchMadeForYouSongs();
+    fetchTrendingSongs();
+  },[fetchFeaturedSongs, fetchMadeForYouSongs, fetchTrendingSongs]);
+
+  console.log({isLoading, madeForYouSongs, featuredSongs, trendingSongs})
+
   return (
-    <div><Topbar /></div>
+    <div className='rounded-md overflow-hidden'>
+      <Topbar />
+    </div>
   )
 }
 
-export default Home
+export default Home 
