43a7036d3a7d7b63b2bdb23c0a74e2fb1fdac603
dhineshrk <dhineshrk77@gmail.com>
2025-01-15 18:53:00 +0530

    Finished Home Page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,24 +1,52 @@
-import Topbar from '@/components/Topbar'
-import { useMusicStore } from '@/stores/useMusicStore'
-import { useEffect } from 'react';
+import Topbar from "@/components/Topbar";
+import { useMusicStore } from "@/stores/useMusicStore";
+import { useEffect } from "react";
+import FeaturedSection from "./components/FeaturedSection";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import SectionGrid from "./components/SectionGrid";
+import { usePlayerStore } from "@/stores/usePlayerStore";
 
-const Home = () => {
+const HomePage = () => {
+	const {
+		fetchFeaturedSongs,
+		fetchMadeForYouSongs,
+		fetchTrendingSongs,
+		isLoading,
+		madeForYouSongs,
+		featuredSongs,
+		trendingSongs,
+	} = useMusicStore();
 
-  const {fetchFeaturedSongs, fetchMadeForYouSongs, fetchTrendingSongs, isLoading, madeForYouSongs, featuredSongs, trendingSongs } =  useMusicStore();
+	const { initializeQueue } = usePlayerStore();
 
-  useEffect(() => {
-    fetchFeaturedSongs();
-    fetchMadeForYouSongs();
-    fetchTrendingSongs();
-  },[fetchFeaturedSongs, fetchMadeForYouSongs, fetchTrendingSongs]);
+	useEffect(() => {
+		fetchFeaturedSongs();
+		fetchMadeForYouSongs();
+		fetchTrendingSongs();
+	}, [fetchFeaturedSongs, fetchMadeForYouSongs, fetchTrendingSongs]);
 
-  console.log({isLoading, madeForYouSongs, featuredSongs, trendingSongs})
+	useEffect(() => {
+		if (madeForYouSongs.length > 0 && featuredSongs.length > 0 && trendingSongs.length > 0) {
+			const allSongs = [...featuredSongs, ...madeForYouSongs, ...trendingSongs];
+			initializeQueue(allSongs);
+		}
+	}, [initializeQueue, madeForYouSongs, trendingSongs, featuredSongs]);
 
-  return (
-    <div className='rounded-md overflow-hidden'>
-      <Topbar />
-    </div>
-  )
-}
+	return (
+		<main className='rounded-md overflow-hidden h-full bg-gradient-to-b from-zinc-800 to-zinc-900'>
+			<Topbar />
+			<ScrollArea className='h-[calc(100vh-180px)]'>
+				<div className='p-4 sm:p-6'>
+					<h1 className='text-2xl sm:text-3xl font-bold mb-6'>Good afternoon</h1>
+					<FeaturedSection />
 
-export default Home 
+					<div className='space-y-8'>
+						<SectionGrid title='Made For You' songs={madeForYouSongs} isLoading={isLoading} />
+						<SectionGrid title='Trending' songs={trendingSongs} isLoading={isLoading} />
+					</div>
+				</div>
+			</ScrollArea>
+		</main>
+	);
+};
+export default HomePage;
